720aae991fd1520e90c04288ec62c07f473cc005
fire1fly <danyseidev@gmail.com>
2022-08-06 05:07:46 +0800

    _

--- src/App.js
+++ src/App.js
@@ -1,8 +1,27 @@
+import React from 'react';
+
+import './assets/styles/app.sass'
+
+import { Header, Categories, Sort, PizzaBlock } from './components'
 
 function App() {
   return (
-    <div className="App">
-      <h1>Dojo Pizza</h1>
+    <div className="wrapper">
+      <Header />
+      <div className="content">
+        <div className="container">
+          <div className="content__top">
+            <Categories />
+            <Sort />
+          </div>
+          <h2 className="content__title">Пицца</h2>
+          <div className="content__items">
+            {
+              Array(10).fill('').map((a, i) => <PizzaBlock key={i} />)
+            }
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
