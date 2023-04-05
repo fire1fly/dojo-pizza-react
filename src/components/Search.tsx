719484a456f4e52aafbf19e66285548ba539d314
fire1fly <danyseidev@gmail.com>
2023-04-05 21:30:46 +0800

    refactor(search): add types for component, props, functions, hooks

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -2,32 +2,32 @@ import React, { useState, useRef, useCallback } from 'react';
 
 import iconSearch from '../assets/media/icon-search.svg';
 import iconClear from '../assets/media/icon-clear.svg';
-import debounce from 'lodash.debounce';
+import { debounce } from 'lodash';
 import { useDispatch } from 'react-redux';
 import { changeSearchQuery } from '../store/filterSlice';
 
 export default function Search() {
 
-  let [localValue, setLocalValue] = useState('');
+  let [localValue, setLocalValue] = useState<string>('');
 
   const dispatch = useDispatch();
 
-  const searchInput = useRef(null);
+  const searchInput = useRef<HTMLInputElement>(null);
 
   const handleSearchIcon = () => {
     setLocalValue('');
     delayCallUpdateSearch('');
-    searchInput.current.focus();
+    searchInput.current?.focus();
   }
 
   
   const delayCallUpdateSearch = useCallback(
-    debounce((value) => {
+    debounce((value: string) => {
       dispatch(changeSearchQuery(value))
     }, 600),
   []);
 
-  const handleSearchInput = e => {
+  const handleSearchInput = (e: React.ChangeEvent<HTMLInputElement>) => {
     setLocalValue(e.target.value);
     delayCallUpdateSearch(e.target.value);
   };
