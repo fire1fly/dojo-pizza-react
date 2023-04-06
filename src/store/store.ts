b240bb478c905f5c01acc285ce711434dce243c5
fire1fly <danyseidev@gmail.com>
2023-04-06 18:25:36 +0800

    refactor(store): add types for redux store and dispatches

--- src/store/store.ts
+++ src/store/store.ts
@@ -2,6 +2,7 @@ import { configureStore } from "@reduxjs/toolkit";
 import filter from "./filterSlice";
 import cart from "./cartSlice";
 import products from "./productsSlice";
+import { useDispatch } from "react-redux";
 
 
 export const store = configureStore({
@@ -11,4 +12,8 @@ export const store = configureStore({
     cart,
   }
 });
+
+export type RootState = ReturnType<typeof store.getState>;
+type AppDispatch = typeof store.dispatch
+export const useAppDispatch: () => AppDispatch = useDispatch
 
