e05c7d04b7a67752e05d8bcc051509c01d1df3f5
fire1fly <danyseidev@gmail.com>
2023-04-06 18:24:28 +0800

    refactor(utils): add types for wordEndingByNumber function

--- src/utils/wordEndingByNumber.ts
+++ src/utils/wordEndingByNumber.ts
@@ -1,4 +1,4 @@
-function wordEndingByNumber(number, words) {
+function wordEndingByNumber(number: number, words: string[]): string {
   return words[(number % 100 > 4 && number % 100 < 20) ? 2 : [2, 0, 1, 1, 1, 2][(number % 10 < 5) ? Math.abs(number) % 10 : 5]];
 }
 
